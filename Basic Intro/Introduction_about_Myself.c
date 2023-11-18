f45021990d41fbe727d13c1060324441eb9d3d6e
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-11-18 12:36:14 +0530

    Update and rename Introduction about Myself to Introduction_about_Myself.c
    Introduction about myself.

---
+++ Basic Intro/Introduction_about_Myself.c
@@ -0,0 +1,5 @@
+// Hello World :)
+// I am Amulya Jain.
+// I am an undergraduate 1st year college student pursuing Bachelors of Technology in Computer Science at University of Petroleum and Energy Studies (UPES), Dehradun, Uttarakhand, India.
+// I am new to coding and GitHub. 
+// Let's learn and build intersting programs by honing skills in Problem Solving and creating Projects using C Programming language.
