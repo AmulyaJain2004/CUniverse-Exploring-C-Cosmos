07b16ed7decf5879b22676296366988edceca9b3
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-11-21 10:45:06 +0530

    Create m_Triangle_n_rows_same_alphabet_columnwise_incrementing_pattern.c
    Application of Nested loops: Pattern series.

---
+++ Basics of C language/g_Interesting_Patterns /m_Triangle_n_rows_same_alphabet_columnwise_incrementing_pattern.c
@@ -0,0 +1,26 @@
+/* Traingle "number same rowwise"
+  Pattern for n=5 is   A
+                       B B
+                       C C C
+                       D D D D
+                       E E E E E
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    char ch =64;
+    
+    printf("Enter the number of rows \n");
+    scanf("%d",&n);
+
+    for (int i = 1; i<=n; i++ )
+    {
+        for(int j = 1; j<=i; j++)
+        {
+            printf("%c ",ch + i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
