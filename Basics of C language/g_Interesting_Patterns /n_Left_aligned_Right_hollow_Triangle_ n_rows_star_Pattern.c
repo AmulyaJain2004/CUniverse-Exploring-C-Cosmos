c265b36074371edc740b955c72f6e427cca3d4f4
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-11-22 00:50:08 +0530

    Create n_Left_aligned_Right_hollow_Triangle_ n_rows_star_Pattern.c
    Application of nested loops and If-Else Conditions: Advanced Pattern series.

---
+++ Basics of C language/g_Interesting_Patterns /n_Left_aligned_Right_hollow_Triangle_ n_rows_star_Pattern.c
@@ -0,0 +1,32 @@
+/*Left aligned Right hollow Triangle 
+  Pattern for n=5 is  * 
+                      * * 
+                      *   *
+                      *     *
+                      * * * * * 
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    
+    printf("Enter the number of rows \n");
+    scanf("%d",&n);
+
+    for (int i = 1; i<=n ; i++ )
+    {
+        for(int j = 1; j<=i; j++)
+        {
+            if ((i==n)||(j==1)||(j==i))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
