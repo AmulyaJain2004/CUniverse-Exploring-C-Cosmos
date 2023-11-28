351998111b719691a33687446767a8002395438d
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-11-28 17:36:54 +0530

    Update a_Pointer.c
    Fundamentals of Pointers

--- Basics of C language/f_Pointers/a_Pointer.c
+++ Basics of C language/f_Pointers/a_Pointer.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the number: -\n");
+    scanf("%d",&x);
+    int *y;
+    y=&x;
+    printf("address of number using &: %d\n",&x);
+    printf("address of number using pointer variable that stored that number: %d\n",y);
+    printf("address of number using pointer variable with &*: %d\n",*&y);
+    printf("value of number using variable: %d\n", x);
+    printf("value of number using pointer variable and *: %d\n",*y);
+    return 0;
+}
