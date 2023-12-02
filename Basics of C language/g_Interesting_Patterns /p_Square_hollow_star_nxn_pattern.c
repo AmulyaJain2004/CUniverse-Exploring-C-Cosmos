71a627acf40a518090aa082def142a3f84f23381
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-03 01:07:35 +0530

    Create p_Square_hollow_star_nxn_pattern.c
    Application of Nested loops and if-else conditions: Advance Pattern series.

---
+++ Basics of C language/g_Interesting_Patterns /p_Square_hollow_star_nxn_pattern.c
@@ -0,0 +1,32 @@
+/* Triangle "number same rowwise"
+  Pattern is  * * * * *
+              *       *
+              *       *
+              *       *
+              * * * * *  */
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    
+    printf("Enter the number of rows \n");
+    scanf("%d",&n);
+
+    for (int i = 1; i<=n ; i++ )
+    {
+        for(int j = 1; j<=n; j++)
+        {
+            if ((i==1)||(i==n)||(j==1)||(j==n))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
