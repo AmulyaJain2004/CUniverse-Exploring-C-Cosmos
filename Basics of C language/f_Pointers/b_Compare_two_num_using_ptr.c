7efefb0ba0e4360691f0fa0e0aa941d8a1c4958f
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-09 11:24:13 +0530

    Create b_Compare_two_num_using_ptr.c
    Comparison of Two numbers using Pointers

---
+++ Basics of C language/f_Pointers/b_Compare_two_num_using_ptr.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the first number: -\n");
+    scanf("%d",&a);
+    printf("Enter the second number: -\n");
+    scanf("%d",&b);
+    int *x =&a;
+    int *y =&b;
+    if (*x > *y)
+    {
+        printf("%d is greater", a);
+    }
+    else if (*x < *y)
+    {
+        printf("%d is greater", b);
+    }
+    else
+    {
+        printf("both are equal");
+    }
+    return 0;
+}
