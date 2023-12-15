1f49629681a7eeb4d0b5fcb50b34bd68161dbf3d
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-15 14:23:37 +0530

    Create b_helloworld_using_file.c
    Displaying Hello World from a file using C programming.

---
+++ Basics of C language/l_File_Handling_Macros_&_Standard_Header_files/b_Displaying_content_using_files/b_helloworld_using_file.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    FILE *fp;
+    char ch; 
+    fp =fopen("hello_world.txt", "r");
+    while(1)
+    {
+        ch = fgetc(fp);
+        if (ch==EOF)
+        {
+            break;
+        }
+        printf("%c",ch);
+    }
+    printf("\n");
+    fclose(fp);
+    return 0;
+}
