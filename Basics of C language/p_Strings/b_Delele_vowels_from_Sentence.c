192d35f04aba4a89e582b86c74d9961e8fd6b3f7
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-17 17:21:54 +0530

    Create b_Delele_vowels_from_Sentence.c
    Program to Delete vowels from Sentence given by user.

---
+++ Basics of C language/p_Strings/b_Delele_vowels_from_Sentence.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LENGTH 80
+
+void Delete_vowel(char *ref , char *tochange)
+{
+    while (*ref != '\0')
+    {
+        if ((*ref == 'a')||(*ref == 'A')||
+            (*ref == 'e')||(*ref == 'E')||
+            (*ref == 'i')||(*ref == 'I')||
+            (*ref == 'o')||(*ref == 'O')||
+            (*ref == 'u')||(*ref == 'U'))
+        {
+            ref++;
+        }
+        else
+        {
+            *tochange++ = *ref++;
+        }
+    }
+
+    *tochange = '\0';
+    printf("Sentence after removing all vowels are: -\n");
+
+}
+
+int main()
+{
+    char *sentence = (char*)malloc(MAX_LENGTH*sizeof(char));
+    printf("Enter a sentence of maximum 80 characters: -\n");
+    gets(sentence);
+
+    char *new_sen = (char*)malloc(MAX_LENGTH*sizeof(char));
+    Delete_vowel(sentence, new_sen);
+    puts(new_sen);
+
+    free(new_sen);
+    free(sentence);
+    return 0;
+}
