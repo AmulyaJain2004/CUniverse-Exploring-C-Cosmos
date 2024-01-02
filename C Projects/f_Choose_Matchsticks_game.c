f5000eaa8469b48987afac8e33072d3edc38f3df
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-02 14:57:31 +0530

    Update f_Choose_Matchsticks_game.c
    Matchsticks Game using Loops, Macros, Conditional Statement in C.
    The Game will be played between the Computer and You.
    There are 21 Matchsticks.
    The Computer asks you to pick 1, 2, 3 or 4 matchsticks.
    Then, the Computer chooses to pick 1, 2, 3 or 4 matchsticks.
    Whoever is forced to pick up the last matchstick loses the game.

--- C Projects/f_Choose_Matchsticks_game.c
+++ C Projects/f_Choose_Matchsticks_game.c
@@ -29,14 +29,14 @@ int Game(int matches_num, int choice)
 
         matches_num -= user_choice;
 
-        printf("\n\n Number of Matches left = %d.\n", matches_num);
+        printf("\n\nNumber of Matches left = %d.\n", matches_num);
         computer_choice = 5 - user_choice;
         printf("Out of given choices, the Computer picked %d matchstick(s).\n", computer_choice);
         matches_num -= computer_choice;
 
         if (matches_num == 1)
         {
-            printf("\n\n Number of Matches left = %d.\n", matches_num);
+            printf("\n\nNumber of Matches left = %d.\n", matches_num);
             printf("You lost the game !!\n");
             printf("############################ GAME OVER ############################");
             break;
@@ -61,14 +61,14 @@ int main()
 
     printf("~~~~~~~~~~~~~~~~ BEST OF LUCK !! ~~~~~~~~~~~~~~~~\n\n");
 
-    printf("\t\t Do you want to play ? \n\t\tEnter 1 for \"YES\" \n\t\tEnter 0 for \"NO\"\n");
+    printf("\t\tDo you want to play ? \n\t\tEnter 1 for \"YES\" \n\t\tEnter 0 for \"NO\"\n");
     scanf("%d", &play);
 
     do
     {
         Game(matchsticks_num, play);
 
-        printf("\n\t\t Game Ended, Do you want to play again? \n\t\tEnter 1 for \"YES\" \n\t\tEnter 0 for \"NO\"\n");
+        printf("\n\t\tGame Ended, Do you want to play again? \n\t\tEnter 1 for \"YES\" \n\t\tEnter 0 for \"NO\"\n");
         scanf("%d", &play);
         if (play == 0)
         {
