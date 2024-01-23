b996071f38a75330c190c056a1af852493eea232
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-23 14:11:49 +0530

    Update a_Bubble_Sort.c
    Program to sort the given array using Bubble Sort Algorithm.

--- Basics of C language/n_Sorting_Algorithms /a_Bubble_Sort.c
+++ Basics of C language/n_Sorting_Algorithms /a_Bubble_Sort.c
@@ -1,1 +1,41 @@
+#include<stdio.h>
 
+int bubble_sort(int arr[], int size)
+{
+    for (int i =0; i<size; i++)
+    {
+        for (int j =0; j<size-i-1; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
+    for (int i = 0 ; i<size; i++)
+    {
+        printf("%d",arr[i]);
+    }
+}
+
+int main()
+{
+    int arr[] = {5,7,8,4,1,9,0,2,3,6};
+    int size_arr = sizeof(arr)/sizeof(arr[0]);
+
+    printf("The original array is: -\n");
+    for (int i = 0; i<size_arr; i++)
+    {
+        printf("%d",arr[i]);
+    }
+    printf("\n");
+
+    printf("The sorted array: -\n");
+    bubble_sort(arr,size_arr);
+
+    return 0 ;
+}
