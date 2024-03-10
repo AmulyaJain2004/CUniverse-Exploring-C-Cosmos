57f84c5ec3ed100feb93521020642d59c19225a2
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-03-10 14:53:54 +0530

    Update b_create_linked_lists_using_loops.c
    Program to create, input values given by values inside n Linked Lists and displaying them using loops.

--- Basics of C language/r_Linked_Lists /b_create_linked_lists_using_loops.c
+++ Basics of C language/r_Linked_Lists /b_create_linked_lists_using_loops.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+int main()
+{
+    head = (struct node*)malloc(sizeof(struct node));
+
+    struct node *temp = head;
+
+    int n;
+    printf("Enter the number of entries you want to make: -\n");
+    scanf("%d",&n);
+
+    printf("Enter the element you want to enter: -\n");
+    scanf("%d",&head->data);
+
+    for (int i = 1; i < n; i++)
+    {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+        temp->next = newnode;
+
+        int a;
+        printf("Enter the element you want to enter: -\n");
+        scanf("%d",&a);
+
+        newnode->data = a;
+        newnode->next = NULL;
+
+        temp = newnode;
+    }
+
+    temp = head;
+
+    while (temp->next != NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    printf("%d",temp->data);
+    return 0;
+}
