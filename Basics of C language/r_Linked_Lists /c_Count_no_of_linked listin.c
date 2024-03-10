caefd9b65cc3347b08238b44a29ba3932bec3f06
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-03-11 00:46:36 +0530

    Create c_Count_no_of_linked listin.c
    Program to calculate number of nodes in linked lists created so far.

---
+++ Basics of C language/r_Linked_Lists /c_Count_no_of_linked listin.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+
+int main()
+{
+    struct node *head = (struct node*)malloc(sizeof(struct node)); 
+    struct node *temp = head;
+
+    int n;
+    printf("Enter the number of entries you want to enter: -\n");
+    scanf("%d",&n);
+
+    printf("enter the data ");
+    scanf("%d",&head->data);
+
+    for (int i= 1; i<n; i++)
+    {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+        temp->next = newnode;
+
+        int d;
+        printf("enter the data ");
+        scanf("%d",&d);
+
+        newnode->data = d;
+        newnode->next = NULL;
+
+        temp = newnode;
+    }
+
+    temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n The number of nodes in this linked list is: %d",count);
+    return 0;
+}
