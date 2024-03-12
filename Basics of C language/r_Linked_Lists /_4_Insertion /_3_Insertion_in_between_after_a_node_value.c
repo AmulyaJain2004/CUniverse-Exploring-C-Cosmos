93b8aa847879d1dddc278b980592f1415f36c3d3
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-03-12 10:23:54 +0530

    Update _3_Insertion_in_between_after_a_node_value.c
    Program to insert a new node after a user entered data of an existing node.

--- Basics of C language/r_Linked_Lists /_4_Insertion /_3_Insertion_in_between_after_a_node_value.c
+++ Basics of C language/r_Linked_Lists /_4_Insertion /_3_Insertion_in_between_after_a_node_value.c
@@ -1,1 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node 
+{
+    int data;
+    struct node *next;
+};
+
+
+int main()
+{
+    struct node *head = (struct node *)malloc (sizeof (struct node));
+    struct node *temp = head;
+
+    int n;
+    printf("Enter the number of entries you want to enter: -\n");
+    scanf("%d",&n);
+
+
+    for (int i = 0 ; i<n; i++)
+    {
+        if (i == 0 )
+        {
+            printf("Enter the data: -\n");
+            scanf("%d",&head->data);
+        }
+        else
+        {
+            struct node *newnode = (struct node *)malloc (sizeof(struct node));
+
+            temp->next = newnode;
+
+            printf("Enter the data: -\n");
+            scanf("%d",&newnode->data);
+            newnode->next = NULL;
+
+            temp = newnode;
+        }
+    }
+    
+    temp = head;
+    printf("The elements of linked list are: -\n");
+    while (temp != NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    
+    // we can put inside do_while loop or while loop also
+    int val;
+    printf("\nEnter value of the node after which new node is to be inserted: -\n");
+    scanf("%d",&val);
+
+    temp = head;
+    while (temp->data != val) 
+    {
+        temp = temp->next;
+    }
+
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data for new node: -\n");
+    scanf("%d",&new_node->data);
+    new_node->next = temp->next;
+    temp->next = new_node;
+
+    temp = head;
+    printf("The elements of new linked list are: -\n");
+    while (temp != NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+
+    // Free the allocated memory to avoid memory leaks
+    temp = head;
+    while (temp != NULL)
+    {
+        struct node *nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+    return 0;
+}
