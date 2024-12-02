d46fcc95e744c1a8f6e90bf93b78e49335fd3ddb
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-12-03 00:00:02 +0530

    Create NoteToMyself.c

---
+++ Basic Intro /NoteToMyself.c
@@ -0,0 +1,5 @@
+// Issues: -
+// Improve the repository structure in structured format with proper readme.
+// Add more codes related to every data structure, advanced c programming.
+// Lots of Implementation projects for every concept.
+// Link it to UPES repository.
